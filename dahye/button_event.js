ea582278603cfa7703c11e10d5b14714b2752f9c
DESKTOP-P5BKBRK\MH-Lee <ekgp2163@naver.com>
2024-04-14 21:31:21 +0900

    숫자 버튼 생성

--- dahye/button_event.js
+++ dahye/button_event.js
@@ -1,5 +1,5 @@
-function btnNum() {
-    let text = "";
+let text = "";
+function btn_num() {
     for (let i = 0; i < 10; i++) {
         // Id = 지훈 인터페이스 숫자 값
         document.getElementById(`btn${i}`).addEventListener("click",
@@ -9,6 +9,57 @@ function btnNum() {
                 document.getElementById("display").innerHTML = text;
             })
     }
-    
+
 }
-btnNum()
+
+// 덧셈 버튼
+let operate_plus = document.getElementById("btn43").addEventListener("click",
+    function () {
+        text += "+";
+        document.getElementById("display").innerHTML = text;
+    })
+
+// 뺄셈 버튼
+let operate_minus = document.getElementById("btn45").addEventListener("click",
+    function () {
+        text += "-";
+        document.getElementById("display").innerHTML = text;
+    })
+
+// 곱하기 버튼
+let operate_multiple = document.getElementById("btn88").addEventListener("click",
+    function () {
+        text += "*";
+        document.getElementById("display").innerHTML = text;
+    })
+
+// 나누기 버튼
+let operate_divide = document.getElementById("btn47").addEventListener("click",
+    function () {
+        text += "/";
+        document.getElementById("display").innerHTML = text;
+    })
+
+// 점 버튼
+let operate_dot = document.getElementById("btn46").addEventListener("click",
+    function () {
+        text += ".";
+        document.getElementById("display").innerHTML = text;
+    })
+
+// 지우기 버튼
+let operate_delete = document.getElementById("btn98").addEventListener("click",
+    function () {
+        // text -= "";
+        document.getElementById("display").innerHTML = text;
+    })
+
+// C 버튼
+let operate_clear = document.getElementById("btn99").addEventListener("click",
+    function () {
+        text = "";
+        document.getElementById("display").innerHTML = text;
+    })
+
+
+btn_num()
