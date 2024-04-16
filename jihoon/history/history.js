5fe10a821e050b00eba870bdacf721d7f423bace
DH(green) <ekgp2163@naver.com>
2024-04-16 19:47:15 +0900

    버튼-히스토리 연결 중

--- jihoon/history/history.js
+++ jihoon/history/history.js
@@ -1,59 +1,79 @@
-
-// 외부로 부터 더미데이터 받음
-let calc_storage =["1+1=2", "2+2=4","2+8+7=17"]
-    // ["1+1=2", "2+2=4"]
-
-let modify = "4+4="
-let result = "8"
-let connected_modify = modify + result
+// import { equal_cal, equal_bt_click } from '../../soohyung/equal_button.js';
+import { equal_bt_click, text } from '../../dahye/button_event.js';
 
 
-calc_storage.push(connected_modify)
-let send =[]
 
-for( x of calc_storage){
-    window.localStorage.setItem(calc_storage.indexOf(x), x)
 
-}
+const history = {
 
-for (let i=0; i < localStorage.length; i++) {
+    // modify: text,
 
-    let call = window.localStorage.getItem(i)
-    create_button(i, call)
-}
+    // result: equal,
 
-function create_button(id_set, text) {
-    
-    const El = document.createElement("button");
-    const Text = document.createTextNode(text)
-    El.appendChild(Text)
-    El.id = `btn${id_set}`
-    let Div =document.getElementById("history_container")
-    Div.appendChild(El)
-    document.body.appendChild(Div)
-    
-}
+    connected_modify: text + equal_bt_click(text),
 
-for (let i = 0; i<localStorage.length ; i++) {
-    const El = document.querySelector(`#btn${i}`)
-    El.addEventListener("click", function(){
-        const local_deta = window.localStorage.getItem(i);
-        const local_data = local_deta.split("=")
-        local_data[0]= local_data[0]+"="
-        
-        send= local_data
-        
+    local_push: function (connected_modify, calc_storage) {
+        calc_storage.push(connected_modify)
+
+        for (x of calc_storage) {
+            window.localStorage.setItem(calc_storage.indexOf(x), x)
+        }
+    },
+    local_get: function () {
+        const El_c = document.createElement("div")
+        El_c.id = "history_container"
+        document.body.appendChild(El_c)
+        for (let i = 0; i < localStorage.length; i++) {
+            let call = window.localStorage.getItem(i)
+            create_button(i, call)
+        }
+
+        function create_button(id_set, text) {
+
+            const El = document.createElement("button");
+            const Text = document.createTextNode(text)
+            El.appendChild(Text)
+            El.id = `btn${id_set}`
+            let Div = document.getElementById("history_container")
+            Div.appendChild(El)
+            document.body.appendChild(Div)
+        }
+    },
+    button_utill: function () {
+        for (let i = 0; i < localStorage.length; i++) {
+            const El = document.querySelector(`#btn${i}`)
+            El.addEventListener("click", function () {
+                const local_deta = window.localStorage.getItem(i);
+                const local_data = local_deta.split("=")
+                local_data[0] = local_data[0] + "="
+                send = local_data
+            })
+        }
+    },
+    delete_button: function () {
+        const El = document.createElement("button")
+        const text = document.createTextNode("DELETE")
+        El.appendChild(text)
+        El.id = "history_delete"
+        document.body.appendChild(El)
+        const delete_button = document.getElementById("history_delete")
+        delete_button.addEventListener("click", () => {
+            window.localStorage.clear()
+            document.querySelector("#history_container").innerHTML = ""
+        })
     }
-    )
 }
 
-const delete_button = document.getElementById("history_delete")
-delete_button.addEventListener("click", () => {
-    window.localStorage.clear()
-    document.querySelector("#history_container").innerHTML = ""    
-    // let button = document.querySelector('[id^="btn"]')
-    // button.remove()
+function history_main(connected_modify) {
+    // let calc_storage = ["1+2=4", "2+5=7"]
+    // connected_modify = "3-2=1"
+    history.connected_modify
+    history.local_push(connected_modify, calc_storage)
+    history.local_get()
+    history.button_utill()
+    history.delete_button()
 
-    
-})
 
+}
+
+history_main()
