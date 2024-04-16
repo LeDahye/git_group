3ce876011453ffd33d792fa70efdba6adfdf48cb
ysw1208 <tony605a@naver.com>
2024-04-16 11:49:21 +0900

    계산기 객체변경

--- sonwong/display.js
+++ sonwong/display.js
@@ -1,44 +1,41 @@
-// 디스플레이 생성
-let display1 = document.createElement('div');
-let display2 = document.createElement('div');
+class Calculator {
+    constructor() {
+        this.display1 = document.createElement('div');
+        this.display2 = document.createElement('div');
+        this.btn1 = document.createElement('button');
+        this.btn2 = document.createElement('button');
 
-display1.id = 'display1';
-display2.id = 'display2';
+        this.display1.id = 'display1';
+        this.display2.id = 'display2';
+        this.btn1.id = 'btn1';
+        this.btn2.id = 'btn2';
 
-document.body.appendChild(display1);
-document.body.appendChild(display2);
+        this.btn1.textContent = 'Display1 버튼';
+        this.btn2.textContent = 'Display2 버튼';
 
-// // 버튼생성
-// let btn1 = document.createElement('button');
-// let btn2 = document.createElement('button');
+        this.btn1.addEventListener('click', () => this.show(1)); // display1에 연결
+        this.btn2.addEventListener('click', () => this.show(2)); // display2에 연결
 
-// btn1.id = 'btn1';
-// btn2.id = 'btn2';
-
-// document.body.appendChild(btn1);
-// document.body.appendChild(btn2);
-
-// 클릭이벤트(수식창,display1)
-btn1.addEventListener('click', function() {
-    let text = 1;
-    let d1 = display1.innerHTML;
-    let d2 = display2.innerHTML;
-
-// 조건문(연산시 d2삭제,기존 d2값 d1으로 이동)
-    if (d1 !== '' && d2 !== '') {
-        let display2Text = document.getElementById('display2').innerHTML;
-        document.getElementById('display1').innerHTML = null;
-        document.getElementById('display1').innerHTML += display2Text;
-        document.getElementById('display2').innerHTML = null;
+        document.body.appendChild(this.display1);
+        document.body.appendChild(this.display2);
+        document.body.appendChild(this.btn1);
+        document.body.appendChild(this.btn2);
     }
-        else {
-        document.getElementById('display1').innerHTML += text;
+
+    show(value) {
+        if (value === 1) {
+            let display1Text = this.display1.innerHTML;
+            if (display1Text !== '' && this.display2.innerHTML !== '') {
+                this.display1.innerHTML = this.display2.innerHTML;
+                this.display2.innerHTML = '';
+            } else {
+                this.display1.innerHTML += value;
+            }
+        } else if (value === 2) {
+            this.display2.innerHTML += value;
+        }
     }
-    });
+}
 
-//클릭이벤트(결과창,display2)
-btn2.addEventListener('click', function () {
-    let text2 = 2;
-    document.getElementById('display2').innerHTML += text2;
-});
-
+// Calculator 객체 생성
+const calculator1 = new Calculator();
