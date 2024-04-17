98fea33e2b15578d487076e5391c3fabb04ba0de
DESKTOP-DAIRV42\차지훈 <jihoon9998@naver.com>
2024-04-18 02:31:05 +0900

    t

--- index/index.js
+++ index/index.js
@@ -3,6 +3,8 @@ import { btn_num_click, operator } from '../dahye/button_event.js';
 import { equal_bt_click } from '../soohyung/equal_button.js';
 import make_display from '../sonwong/display.js';
 
+
+
 ui()
 make_display()
 btn_num_click()
@@ -10,7 +12,9 @@ operator()
 
 
 document.getElementById("btn61").addEventListener("click", function () {
-    let display2 = document.getElementById("display2").innerHTML
-    let result = equal_bt_click(display2);
-    document.getElementById("display1").innerHTML = result
+let modify = document.getElementById("display2").innerHTML
+let result = equal_bt_click(modify);
+document.getElementById("display1").innerHTML = result
 })
+
+
