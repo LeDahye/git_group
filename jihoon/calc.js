d8d2f1950e4a9fb34f995723af130a840e43bd4a
DESKTOP-DAIRV42\차지훈 <jihoon9998@naver.com>
2024-04-18 01:08:50 +0900

    수정

--- jihoon/calc.js
+++ jihoon/calc.js
@@ -3,25 +3,12 @@ class Make_box{
     constructor(type,El_name,parent) {
         this.type = type;
         this.El_name = El_name
+        this.parent = parent
     }
     make() {
         const El = document.createElement(this.type)
-        El.id = this.El_name
-        document.body.appendChild(El)
-    }
-}
-
-//박스 삽입 클래스
-class insert_box{
-    constructor(parent, child) {
-        this.parent = parent;
-        this.child = child;
-    }
-    insert_box() {
-        const parent = document.getElementById(this.parent)
-        const child = document.getElementById(this.child)
-        parent.appendChild(child)
-        
+        El.id = this.El_name   
+        document.querySelector(this.parent).appendChild(El)
     }
 }
 
@@ -47,9 +34,6 @@ class make_num_button{
 //연산자 패드 생성 클래스
 class make_operator_button extends make_num_button{
     constructor(type,className,parent) {
-        // this.type = type
-        // this.className = className
-        // this.parent = parent
         super(type,className,parent)
     }
 
@@ -63,8 +47,7 @@ class make_operator_button extends make_num_button{
             El.id = `btn${x.charCodeAt(0)}`
             El.className = this.className
             document.getElementById(this.parent).appendChild(El)
-            }
-            
+            }     
     }
 }
 
@@ -76,30 +59,21 @@ function css_set() {
 
 
 
-const history_box = new Make_box("div","history_box")
-
-const insert_PinC = new insert_box("container","pad_box")
-
-const pad_box = new Make_box("div", "pad_box")
-
-const main_container = new Make_box("div", "container")
-
+const history_box = new Make_box("div","history_box","#container")
+const pad_box = new Make_box("div", "pad_box","#container")
+const main_container = new Make_box("div", "container","body")
 const num_button = new make_num_button("button","btn","pad_box")
-
 const operator_button = new make_operator_button("button", "btn operate", "pad_box")
-
-const insert_display = new insert_box("container","dis_container")
-
-const display_box = new Make_box("div", "dis_container")
  
-function ui(){
-pad_box.make()
+
+
+function ui() {
+    
 main_container.make()
-insert_PinC.insert_box()
+pad_box.make()
 num_button.make()
 operator_button.make()
-display_box.make()
-insert_display.insert_box()
+history_box.make()
 css_set()
 }
 
