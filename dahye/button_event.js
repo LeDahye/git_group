925a98046817a77d91e8392e1f5a5dfb505ecbe1
DESKTOP-SRV7RUU\home <snf00020@naver.com>
2024-04-17 19:19:22 +0900

    -버튼, 괄호열기 수정

--- dahye/button_event.js
+++ dahye/button_event.js
@@ -56,7 +56,7 @@ function operator() {
             display2.innerHTML = display1.innerHTML;
             display1.innerHTML = '';
             display2.innerHTML += '-';
-        } if (text[text.length - 1] !== '-') {
+        } else if (text[text.length - 1] !== '-') {
             text += "-";
             document.getElementById("display2").innerHTML = text;
         }
@@ -120,15 +120,24 @@ function operator() {
 
     // 괄호 열기 버튼
     document.getElementById("btn40").addEventListener("click", function () {
-        let text = document.getElementById("display2").innerHTML;
-        text += "(";
-        document.getElementById("display2").innerHTML = text;
+        let display1 = document.getElementById("display1");
+        let display2 = document.getElementById("display2");
+        let text = display2.innerHTML;
+        if (display1.innerHTML !== '' && display2.innerHTML !== '') {
+            display2.innerHTML = display1.innerHTML;
+            display1.innerHTML = '';
+            display2.innerHTML += '(';
+        } else if (isNaN(text[text.length - 1])) {
+            text += "(";
+            document.getElementById("display2").innerHTML = text;
+        }
+
     });
 
     // 괄호 닫기 버튼
     document.getElementById("btn41").addEventListener("click", function () {
         let text = document.getElementById("display2").innerHTML;
-        if (text.includes("(") == true) {
+        if (text.includes("(") === true) {
             text += ")";
             document.getElementById("display2").innerHTML = text;
         }
@@ -156,7 +165,7 @@ function operator() {
         if (!isNaN(text[text.length - 1])) {
             text += "=";
             document.getElementById("display2").innerHTML = text;
-        } else if (text.includes(")") == true) {
+        } else if (text.includes(")") === true) {
             text += "=";
             document.getElementById("display2").innerHTML = text;
         }
