49f4c6ecb21cfd3f6f5684d4fb3509d17e610a16
chajihoon(green) <jihoon9998@naver.com>
2024-04-18 17:47:07 +0900

    수정

--- index/index.js
+++ index/index.js
@@ -1,18 +1,18 @@
-import ui from '../jihoon/calc.js'
+import { ui} from '../jihoon/calc.js'
 import {btn_num_click, operator} from '../dahye/button_event.js';
 import {equal_bt_click} from '../soohyung/equal_button.js';
 import make_display from '../sonwong/display.js';
-
+import { history } from '../jihoon/history/history.js';
 
 ui()
 make_display()
 btn_num_click()
 operator()
 
-
 document.getElementById("btn61").addEventListener("click", function () {
     let modify = document.getElementById("display2").innerHTML
     document.getElementById("display1").innerHTML = equal_bt_click(modify)
 })
 
+history()
 
