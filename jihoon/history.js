f99c7738e1e684a48f9740b7cb884144749a6fce
DH(green) <ekgp2163@naver.com>
2024-04-19 12:07:56 +0900

    history 줄 수정

--- jihoon/history.js
+++ jihoon/history.js
@@ -1,29 +1,24 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 86919ae3ef13f312df17556938e9ea2d17924de7
-
 function history() {
 
-    class Make_box{
-    constructor(type,id,parent,className) {
-        this.type = type;
-        this.id = id
-        this.parent = parent
-        this.className=className
-    }
-    make() {
-        const El = document.createElement(this.type)
-        El.id = this.id
-        if (this.className !== undefined) {
-            El.className = this.className
+    class Make_box {
+        constructor(type, id, parent, className) {
+            this.type = type;
+            this.id = id
+            this.parent = parent
+            this.className = className
         }
-        document.querySelector(this.parent).appendChild(El)
+        make() {
+            const El = document.createElement(this.type)
+            El.id = this.id
+            if (this.className !== undefined) {
+                El.className = this.className
+            }
+            document.querySelector(this.parent).appendChild(El)
 
-        return this.id
+            return this.id
         }
-        
-}
+
+    }
 
 
 
@@ -32,7 +27,7 @@ function history() {
     const history_box = new Make_box("div", "history_box", "#container",);
     const delete_button_container = new Make_box("div", "delete_button_container", "#container")
     const delete_button = new Make_box("button", "delete_button", "#delete_button_container",);
-    const delete_imgtag = new Make_box("img", "delete_img","#delete_button")
+    const delete_imgtag = new Make_box("img", "delete_img", "#delete_button")
 
     delete_button_container.make()
     history_box.make()
@@ -40,8 +35,8 @@ function history() {
     delete_imgtag.make()
 
 
-    
-    
+
+
 
     document.querySelector("#btn61").addEventListener("click", () => {
         let modify = document.querySelector("#display2").innerHTML
@@ -51,33 +46,33 @@ function history() {
             history = document.querySelector("#test").innerHTML = modify + result
             history_storage.push(history)
             window.localStorage.setItem(localStorage.length, history_storage.slice(-1))
-            
+
             const El = new Make_box("button", `get_button${localStorage.length - 1}`, "#history_box", "btn_history")
             El.make()
             document.querySelector(`#get_button${localStorage.length - 1}`).innerHTML = history
             let count = window.localStorage.length
-           
-           
+
+
             let temp
-            for (let i = 0; i <=count-1 ;i++){
-                let button = document.querySelector(`#get_button${i}`)    
+            for (let i = 0; i <= count - 1; i++) {
+                let button = document.querySelector(`#get_button${i}`)
                 button.addEventListener("click", () => {
-        
+
                     let text = button.innerHTML
-                    temp=text.split("=",2)
-                    document.querySelector("#display2").innerHTML=temp[0]+"="
-                    document.querySelector("#display1").innerHTML =temp[1]
+                    temp = text.split("=", 2)
+                    document.querySelector("#display2").innerHTML = temp[0] + "="
+                    document.querySelector("#display1").innerHTML = temp[1]
                 })
-                
+
             }
         }
     })
 
-    
 
-    function call_stage(){}
 
-    document.querySelector("#delete_img").setAttribute("src","../jihoon/trash_can.svg")
+    function call_stage() { }
+
+    document.querySelector("#delete_img").setAttribute("src", "../jihoon/trash_can.svg")
     let history_storage = []
 
     for (let i = 0; i <= localStorage.length - 1; i++) {
@@ -86,9 +81,9 @@ function history() {
         El.make()
         document.querySelector(`#get_button${i}`).innerHTML = history
     }
-    
-    
-    
+
+
+
     document.querySelector("#delete_button").addEventListener("click", () => {
         localStorage.clear()
         document.querySelector("#history_box").innerHTML = ""
