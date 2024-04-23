9830b4691fb680d6ac5c77f5044b0cd48601de92
DH(green) <ekgp2163@naver.com>
2024-04-23 11:54:46 +0900

    수정

--- index/index.js
+++ index/index.js
@@ -1,33 +1,21 @@
 import { ui } from '../sonwong/display.js'
 import { btn_num_click, operator, operator2 } from '../dahye/button_event.js';
-
 import { equal_bt_click } from '../soohyung/postfix_calc.js';
 import { history } from '../jihoon/history.js';
 
-
-ui()
-
-btn_num_click()
+ui()    // display 연결
+btn_num_click()   // 연산식 호출
 operator("btn43", "click", "+");    // 덧셈 버튼
 operator("btn45", "click", "-");    // 뺄셈, 음수 버튼
 operator("btn120", "click", "x");   // 곱하기 버튼
 operator("btn38", "click", "÷");    // 나누기 버튼
-operator2();
+operator2();    // 연산자 및 기호 버튼
 
 document.getElementById("btn61").addEventListener("click", function () {
+    // equal 버튼 클릭 시 결과값 도출
     let modify = document.getElementById("display2").innerHTML
     let result = equal_bt_click(modify);
     document.getElementById("display1").innerHTML = result
 })
 
-
-
-
-
-
-// history()
-history()
-
-
-
-
+history()   // history 연결
--- jihoon/history.js
+++ jihoon/history.js
@@ -1,9 +1,9 @@
-    class Make_box{
-    constructor(type,id,parent,className) {
+class Make_box {
+    constructor(type, id, parent, className) {
         this.type = type;
         this.id = id
         this.parent = parent
-        this.className=className
+        this.className = className
     }
     make() {
         const El = document.createElement(this.type)
@@ -15,78 +15,78 @@
         return this.id
     }
 }
-    const history_box = new Make_box("div", "history_box", "#container");
-    const delete_button_container = new Make_box("div", "delete_button_container", "#container")
-    const delete_button = new Make_box("button", "delete_button", "#delete_button_container");
-    const delete_imgtag = new Make_box("img", "delete_img","#delete_button")
-    
-    
-    // "="버튼 클릭이벤트
-    function eq_event() {
-        
-        document.querySelector("#btn61").addEventListener("click", () => {
-            let modify = document.querySelector("#display2").innerHTML
-            let result = document.querySelector("#display1").innerHTML
-            let count = window.localStorage.length
-            if (modify.includes("=") == 1) {
-                let temp_storage = modify + result
-                push_local(temp_storage)
-                
-                //히스토리버튼 생성
-                const history_button = new Make_box("button", `get_button${localStorage.length - 1}`, "#history_box", "btn_history")
-                const history_btn_id = history_button.make()
-                document.querySelector(`#${history_btn_id}`).innerHTML=temp_storage
-                
-                //히스토리버튼 기능 삽입
-                let temp
-                call_history(count,temp)
-            }
+const history_box = new Make_box("div", "history_box", "#container");
+const delete_button_container = new Make_box("div", "delete_button_container", "#container")
+const delete_button = new Make_box("button", "delete_button", "#delete_button_container");
+const delete_imgtag = new Make_box("img", "delete_img", "#delete_button")
+
+
+// "="버튼 클릭이벤트
+function eq_event() {
+
+    document.querySelector("#btn61").addEventListener("click", () => {
+        let modify = document.querySelector("#display2").innerHTML
+        let result = document.querySelector("#display1").innerHTML
+        let count = window.localStorage.length
+        if (modify.includes("=") == 1) {
+            let temp_storage = modify + result
+            push_local(temp_storage)
+
+            //히스토리버튼 생성
+            const history_button = new Make_box("button", `get_button${localStorage.length - 1}`, "#history_box", "btn_history")
+            const history_btn_id = history_button.make()
+            document.querySelector(`#${history_btn_id}`).innerHTML = temp_storage
+
+            //히스토리버튼 기능 삽입
+            let temp
+            call_history(count, temp)
+        }
     })
 }
 //로컬에 데이터 가져오기
-function call_storage(parent,classname) {
-        for (let i = 0; i <= localStorage.length - 1; i++) {
+function call_storage(parent, classname) {
+    for (let i = 0; i <= localStorage.length - 1; i++) {
         const history = window.localStorage.getItem(i)
         const El = new Make_box("button", `get_button${i}`, `#${parent}`, classname)
         El.make()
         document.querySelector(`#get_button${i}`).innerHTML = history
     }
-    
-    
+
+
 }
 //딜리트버튼 삭제기능 삽입
-function deleter(btn_id,box_id) {
+function deleter(btn_id, box_id) {
     document.querySelector(`#${btn_id}`).addEventListener("click", () => {
         localStorage.clear()
         document.querySelector(`#${box_id}`).innerHTML = ""
     })
 }
 //로컬에 데이터 전송
-function push_local(temp_storage){
-    
+function push_local(temp_storage) {
+
     let history_storage = []
-    
+
     history_storage.push(temp_storage)
     window.localStorage.setItem(localStorage.length, history_storage.slice(-1))
 }
 //
-function call_history(count,temp) {
+function call_history(count, temp) {
     for (let i = 0; i <= count; i++) {
         let button = document.querySelector(`#get_button${i}`)
         button.addEventListener("click", () => {
             let text = button.innerHTML
             temp = text.split("=", 2)
-            document.querySelector("#display2").innerHTML=temp[0]+"="
-            document.querySelector("#display1").innerHTML =temp[1]
+            document.querySelector("#display2").innerHTML = temp[0] + "="
+            document.querySelector("#display1").innerHTML = temp[1]
         }
         )
     }
-    
+
 }
 
 
 function history() {
-    
+
     // 히스토리UI 생성
     const history_id = history_box.make();
 
@@ -94,7 +94,7 @@ function history() {
     const delete_btn_con_id = delete_button_container.make();
     const delete_btn_id = delete_button.make();
     const delete_imgtag_id = delete_imgtag.make();
-    
+
     call_storage(history_id, "btn_hitory")
     eq_event()
     //딜리트버튼 기능 삽입
@@ -102,6 +102,6 @@ function history() {
     //히스토리버튼 기능 삽입
     document.querySelector("#delete_img").setAttribute("src", "../jihoon/trash_can.svg")
     let temp
-    call_history(window.localStorage.length,temp)
+    call_history(window.localStorage.length, temp)
 }
-export{history}
+export { history }
